5d926ab02d9c4b115a5934a3eff14a1be1d40306
nosliwa <125345406+a-nosliw@users.noreply.github.com>
2023-05-30 10:35:22 +1000

    lesson 1: basic hello world

---
+++ apps/01_hello_world/you_try/greeting_hello_world.py
@@ -0,0 +1,9 @@
+print('--------------------------------')
+print('          HELLO APP')
+print('--------------------------------')
+print()
+
+user_input = input("What is your name?")
+greeting = "Welcome to learning python"+ user_input
+
+print(greeting)
